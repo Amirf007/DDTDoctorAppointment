0de6eecd3aec31851be33b3a9603c1a987b6bd92
Amir <amirfeyzipoor0007@gmail.com>
2022-04-26 14:28:15 -0700

    Create DDTDotorAppointment.Services.Test.Unit layer

---
+++ src/DDTDoctorAppointment.Services.Test.Unit/Appointments/AppointmentServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDTDoctorAppointment.Services.Test.Unit.Appointments
+{
+    public class AppointmentServiceTests
+    {
+    }
+}
