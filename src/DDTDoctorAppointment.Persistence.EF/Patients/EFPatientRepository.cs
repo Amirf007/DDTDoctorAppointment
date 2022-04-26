556ad14202c1ed5ab5dc598f5bc183c22adf6c45
Amir <amirfeyzipoor0007@gmail.com>
2022-04-26 03:00:33 -0700

    Create DDTDoctorAppointment.Persistence.EF layer

---
+++ src/DDTDoctorAppointment.Persistence.EF/Patients/EFPatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDTDoctorAppointment.Persistence.EF.Patients
+{
+    public class EFPatientRepository
+    {
+
+    }
+}
