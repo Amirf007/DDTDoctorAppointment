0f1460c9edf36f8b312a95954fce04f84916af27
Amir <amirfeyzipoor0007@gmail.com>
2022-04-26 02:23:37 -0700

    Create DDTDoctorAppointment.Entities and DDDoctorAppointment.Insfrastructure and DDTDoctorAppointment.Migration layer

---
+++ src/DDTDoctorAppointment.Entities/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDTDoctorAppointment.Entities
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string DoctorId { get; set; }
+        public string PatientId { get; set; }
+        public DateTime AppointmentTime { get; set; }
+        public Doctor Doctor { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
---
+++ src/DDTDoctorAppointment.Migrations/202204261326_initalTables.cs
@@ -0,0 +1,72 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDTDoctorAppointment.Migrations
+{
+    [Migration(202204261326)]
+    public class _202204261326_initalTables : Migration
+    {
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_Appointments_Patients");
+            Delete.ForeignKey("FK_Appointments_Doctors");
+            Delete.Table("Doctors");
+            Delete.Table("Patients");
+            Delete.Table("Appointments");
+        }
+
+        public override void Up()
+        {
+            CreateDoctorsTable();
+            CreatePatientsTable();
+            CreateAppointmentsTable();
+
+            CreateRelationships();
+        }
+
+        private void CreateRelationships()
+        {
+            Create.ForeignKey("FK_Appointments_Patients")
+                    .FromTable("Appointments").ForeignColumns("PatientId")
+                    .ToTable("Patients").PrimaryColumn("Id")
+                    .OnDeleteOrUpdate(System.Data.Rule.Cascade);
+
+            Create.ForeignKey("FK_Appointments_Doctors")
+                    .FromTable("Appointments").ForeignColumns("DoctorId")
+                    .ToTable("Doctors").PrimaryColumn("Id")
+                    .OnDeleteOrUpdate(System.Data.Rule.Cascade);
+        }
+
+        private void CreateAppointmentsTable()
+        {
+            Create.Table("Appointments")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                    .WithColumn("DoctorId").AsInt32().NotNullable()
+                    .WithColumn("PatientId").AsInt32().NotNullable()
+                    .WithColumn("AppointmentTime").AsDateTime().NotNullable();
+        }
+
+        private void CreatePatientsTable()
+        {
+            Create.Table("Patients")
+                            .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                                .WithColumn("NationalCode").AsString(50).Unique().NotNullable()
+                                .WithColumn("Name").AsString(50).NotNullable()
+                                .WithColumn("LastName").AsString(50).NotNullable();
+        }
+
+        private void CreateDoctorsTable()
+        {
+            Create.Table("Doctors")
+                 .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                .WithColumn("NationalCode").AsString(50).Unique().NotNullable()
+                .WithColumn("Name").AsString(50).NotNullable()
+                .WithColumn("LastName").AsString(50).NotNullable()
+                .WithColumn("Specialty").AsString(50).NotNullable();
+        }
+    }
+}
